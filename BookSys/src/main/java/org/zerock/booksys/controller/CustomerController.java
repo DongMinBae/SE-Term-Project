6e3c8cad201247ea32b9f2e5cca7b638198cf9e4
GitBchan <chan4527@naver.com>
2023-05-08 00:50:49 +0900

    Connection all page

--- BookSys/src/main/java/org/zerock/booksys/controller/CustomerController.java
+++ BookSys/src/main/java/org/zerock/booksys/controller/CustomerController.java
@@ -3,6 +3,7 @@ package org.zerock.booksys.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.booksys.service.CustomerService;
 
@@ -11,6 +12,11 @@ import org.zerock.booksys.service.CustomerService;
 @Log4j2
 @RequiredArgsConstructor
 public class CustomerController {
-
     private final CustomerService customerService;
+
+    @GetMapping("/login")
+    public void login(){
+        log.info("login page");
+    }
+
 }
