9fd1ef824275146c19e499175dcca36c7249ea23
구동혁 <dhku@naver.com>
2023-06-05 18:51:54 +0900

    War 배포 코드 추가

--- BookSys/src/main/java/org/zerock/booksys/BookSysApplication.java
+++ BookSys/src/main/java/org/zerock/booksys/BookSysApplication.java
@@ -2,11 +2,18 @@ package org.zerock.booksys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class BookSysApplication {
+public class BookSysApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BookSysApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookSysApplication.class, args);
